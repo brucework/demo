17581f74369e85597fb9be07140e962a2591545c
bailiwei <liwei.bai@brisky.cc>
2017-08-28 17:33:26 +0800

    Module: Makefile Detial: edit the makefile

--- src/print/print.c
+++ src/print/print.c
@@ -3,10 +3,17 @@
 
 #include <debug.h>
 
+#define ESC    "\033"
+#define UP     "\033[A"
+#define DOWN   "\033[B"
+#define LEFT   "\033[D"
+#define RIGHT  "\033[C"
+
 void main(void)
 {
     while(1)
     {
+        printf("\033[2J");
         sleep(1);
         debug(INFO,"%s enter.\n", __func__);
         debug(WARN,"%s enter.\n", __func__);
@@ -16,6 +23,11 @@ void main(void)
         info("%s enter.\n", __func__);
         warn("%s enter.\n", __func__);
         err("%s enter.\n", __func__);
+
+       printf("\033[8;5Hhello!\n");
+
+
+
 
     }
 }
