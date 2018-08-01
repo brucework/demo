409af6b127709d52a39c1f52a98c60e557d0bd84
bailiwei <bailiwei@smartisan.com>
2018-08-01 10:54:07 +0800

    Tools: update the tools
    Signed-off-by: bailiwei <bailiwei@smartisan.com>

--- tools/install.sh
+++ tools/install.sh
@@ -2,7 +2,24 @@
 
 function install_ctags()
 {
+    echo "Install ctags tools"
     sudo apt-get install ctags
 }
 
+function install_vim()
+{
+    echo "Install vim tools"
+    sudo apt-get install vim vim-scripts vim-gtk vim-gnome
+
+    echo "Config the vim"
+    #cp ./vim/vimrc ~/.vimrc
+}
+
+function install_cscopy()
+{
+    echo "Install cscopy"
+    sudo apt-get install cscopy
+}
+install_vim
 install_ctags
+
