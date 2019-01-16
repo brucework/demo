b4bb827d79908c5f99e3e819ea37f9e8bb83b816
bailiwei <bailiwei@smartisan.com>
2019-01-16 14:37:15 +0800

    Edit the shadowsock-config.sh
    Signed-off-by: bailiwei <bailiwei@smartisan.com>

--- tools/shadowsocks-ubuntu/shadowsock-config.sh
+++ tools/shadowsocks-ubuntu/shadowsock-config.sh
@@ -13,7 +13,17 @@ sudo apt install shadowsocks
 
 ######
 #
-# Add "sslocal -c $HOME/tools/shadowsocks/shadowsocks.json  &" into  $HOME/.profile
+# Add "sslocal -c $HOME/tools/shadowsocks/shadowsocks.json  &" into  $HOME/.profilea
+#
+# #Init the sslocal when startup the system
+# data=`ps -e | grep "sslocal" | awk '{print $4;}'`
+# if [ "$data"x = "sslocal"x ]; then
+#     echo "Don't need init sslocal"
+# else
+#     echo "Should init sslocal"
+#     sslocal -c $HOME/tools/shadowsocks/shadowsocks.json &
+# fi
+#
 # reboot
 #
 ######
