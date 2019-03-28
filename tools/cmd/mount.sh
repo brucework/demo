0c0c84aa874e255535ac0635164a58e9304ad3f3
bailiwei <bailiwei@smartisan.com>
2019-03-28 15:32:27 +0800

    update the mount
    Signed-off-by: bailiwei <bailiwei@smartisan.com>

--- tools/cmd/mount.sh
+++ tools/cmd/mount.sh
@@ -12,5 +12,8 @@ sudo mount -t cifs //172.16.2.17/share /mnt/share_17 -o username=$USER,password=
 echo "mount 172.16.2.17 to /mnt/share_225"
 sudo mount -t cifs //172.16.2.225/SW_Test /mnt/SW_Test_225 -o username=$USER,password=T123.blw,domain=smartisan.cn
 
+echo "mount 172.16.2.17 to /mnt/flash_225"
+sudo mount -t cifs //172.16.2.225/flash /mnt/flash_225 -o username=$USER,password=T123.blw,domain=smartisan.cn
+
 echo "mount 172.16.26.22 to /mnt/flash_22"
 sudo mount -t cifs //172.16.26.22/flash /mnt/flash_22 -o username=bspsystem,password=bspsystem
