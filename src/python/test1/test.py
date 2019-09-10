acffd7650acef8f2a98c1fdea68c6c0fb7cef865
bailiwei <bailiwei@bytedance.com>
2019-09-10 15:56:16 +0800

    Edit the complete error
    Signed-off-by: bailiwei <bailiwei@bytedance.com>

---
+++ src/python/test1/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+print "python myself test!!!!!!!!!!!!!!";
+print 2*7;
+abcd="sdfsfs";
+print "abcd";
+#if( 1 == 1 ) {
+#    print "true";
+#}
+##else
+#{
+#    print "false";
+#}
+
+print "python myself test end!!!!!!!!!!!!!!";
